548c2259b8d0ef2f9c8096ff9d642a8c37b76c26
Gustavo Enndi <gustavo_enndi@hotmail.com>
2018-06-05 04:09:47 -0300

    iniciando projeto

---
+++ ptm/src/main/java/com/ptm/ptm/PtmBackApplication.java
@@ -0,0 +1,12 @@
+package com.ptm.ptm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtmBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtmBackApplication.class, args);
+	}
+}
---
+++ ptm/src/test/java/com/ptm/ptm/PtmBackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ptm.ptm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PtmBackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
